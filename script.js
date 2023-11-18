d2ec7bc35b93de72ddfb7308f5dc35f9eed9f57f
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-11-19 00:14:33 +0530

    Added the relevant API endpoints...

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 const API_URL =
-  "https://api.themoviedb.org/3/movie/discover/movie?sort_by=popularity.desc&api_key=b5885d493bbaf8ab090e423b57f801bc&page=1";
+  "https://api.themoviedb.org/3/movie/top_rated?api_key=b5885d493bbaf8ab090e423b57f801bc&language=en-US&page=1";
+const IMG_URL = "https://image.tmdb.org/t/p/w1280";
+const SEARCH_URL =
+  'https://api.themoviedb.org/3/search/movie?api_key=b5885d493bbaf8ab090e423b57f801bc&query"';
+
+// Getting the initial movies...
+getMovies(API_URL);
+
+async function getMovies(url) {
+  const res = await fetch(url);
+  const data = await res.json();
+  console.log(data.results);
+}
