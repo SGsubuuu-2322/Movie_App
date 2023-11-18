e204fc47f62ebe479a69502da0f6299eadbc79f5
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-11-19 00:40:58 +0530

    Added some scripts for inital and search api data...

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const API_URL =
   "https://api.themoviedb.org/3/movie/top_rated?api_key=b5885d493bbaf8ab090e423b57f801bc&language=en-US&page=1";
-const IMG_URL = "https://image.tmdb.org/t/p/w1280";
-const SEARCH_URL =
-  'https://api.themoviedb.org/3/search/movie?api_key=b5885d493bbaf8ab090e423b57f801bc&query"';
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCH_API =
+  'https://api.themoviedb.org/3/search/movie?api_key=b5885d493bbaf8ab090e423b57f801bc&query="';
+
+const form = document.getElementById("form");
+const search = document.getElementById("search");
 
 // Getting the initial movies...
 getMovies(API_URL);
@@ -12,3 +15,15 @@ async function getMovies(url) {
   const data = await res.json();
   console.log(data.results);
 }
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const searchTerm = search.value;
+
+  if (searchTerm && searchTerm !== "") {
+    getMovies(SEARCH_API + searchTerm);
+    search.value = "";
+  } else {
+    window.location.reload();
+  }
+});
