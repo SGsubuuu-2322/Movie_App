7fbc227e8ae419d176100cab2186015cc93a9bda
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-11-18 23:47:27 +0530

    Added the api for this project...

--- script.js
+++ script.js
@@ -1,1 +1,2 @@
-const main = document.getElementById("main");
+const API_URL =
+  "https://api.themoviedb.org/3/movie/discover/movie?sort_by=popularity.desc&api_key=b5885d493bbaf8ab090e423b57f801bc&page=1";
